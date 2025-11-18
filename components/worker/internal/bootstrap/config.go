b003d8df392eb87830c809d227172712801d6ccd
arthurkz <arthur.ribeiro@lerian.studio>
2025-11-18 13:49:57 -0300

    feat: Adjusting the code creating the worker flow.

---
+++ components/worker/internal/bootstrap/config.go
@@ -0,0 +1,146 @@
+package bootstrap
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/LerianStudio/fetcher/components/worker/internal/adapters/rabbitmq"
+	"github.com/LerianStudio/fetcher/components/worker/internal/services"
+	"github.com/LerianStudio/fetcher/pkg/constant"
+	externalConnection "github.com/LerianStudio/fetcher/pkg/mongodb/externalConnections"
+	simpleClient "github.com/LerianStudio/fetcher/pkg/seaweedfs"
+	"github.com/LerianStudio/fetcher/pkg/seaweedfs/external_data"
+
+	libCommons "github.com/LerianStudio/lib-commons/v2/commons"
+	mongoDB "github.com/LerianStudio/lib-commons/v2/commons/mongo"
+	libOtel "github.com/LerianStudio/lib-commons/v2/commons/opentelemetry"
+	libRabbitMQ "github.com/LerianStudio/lib-commons/v2/commons/rabbitmq"
+	libZap "github.com/LerianStudio/lib-commons/v2/commons/zap"
+	libLicense "github.com/LerianStudio/lib-license-go/v2/middleware"
+)
+
+// Config holds the application's configurable parameters read from environment variables.
+type Config struct {
+	EnvName                     string `env:"ENV_NAME"`
+	LogLevel                    string `env:"LOG_LEVEL"`
+	RabbitURI                   string `env:"RABBITMQ_URI"`
+	RabbitMQHost                string `env:"RABBITMQ_HOST"`
+	RabbitMQPortHost            string `env:"RABBITMQ_PORT_HOST"`
+	RabbitMQPortAMQP            string `env:"RABBITMQ_PORT_AMQP"`
+	RabbitMQUser                string `env:"RABBITMQ_DEFAULT_USER"`
+	RabbitMQPass                string `env:"RABBITMQ_DEFAULT_PASS"`
+	RabbitMQGenerateReportQueue string `env:"RABBITMQ_GENERATE_REPORT_QUEUE"`
+	RabbitMQNumWorkers          int    `env:"RABBITMQ_NUMBERS_OF_WORKERS"`
+	RabbitMQHealthCheckURL      string `env:"RABBITMQ_HEALTH_CHECK_URL"`
+	OtelServiceName             string `env:"OTEL_RESOURCE_SERVICE_NAME"`
+	OtelLibraryName             string `env:"OTEL_LIBRARY_NAME"`
+	OtelServiceVersion          string `env:"OTEL_RESOURCE_SERVICE_VERSION"`
+	OtelDeploymentEnv           string `env:"OTEL_RESOURCE_DEPLOYMENT_ENVIRONMENT"`
+	OtelColExporterEndpoint     string `env:"OTEL_EXPORTER_OTLP_ENDPOINT"`
+	EnableTelemetry             bool   `env:"ENABLE_TELEMETRY"`
+	// SeaweedFS configuration envs
+	SeaweedFSHost      string `env:"SEAWEEDFS_HOST"`
+	SeaweedFSFilerPort string `env:"SEAWEEDFS_FILER_PORT"`
+	SeaweedFSTTL       string `env:"SEAWEEDFS_TTL"`
+	// MongoDB
+	MongoURI        string `env:"MONGO_URI"`
+	MongoDBHost     string `env:"MONGO_HOST"`
+	MongoDBName     string `env:"MONGO_NAME"`
+	MongoDBUser     string `env:"MONGO_USER"`
+	MongoDBPassword string `env:"MONGO_PASSWORD"`
+	MongoDBPort     string `env:"MONGO_PORT"`
+	MaxPoolSize     int    `env:"MONGO_MAX_POOL_SIZE"`
+	// License configuration envs
+	LicenseKey      string `env:"LICENSE_KEY"`
+	OrganizationIDs string `env:"ORGANIZATION_IDS"`
+}
+
+// InitWorker initializes and configures the application's dependencies and returns the Service instance.
+func InitWorker() *Service {
+	cfg := &Config{}
+	if err := libCommons.SetConfigFromEnvVars(cfg); err != nil {
+		panic(err)
+	}
+
+	logger := libZap.InitializeLogger()
+
+	telemetry := libOtel.InitializeTelemetry(&libOtel.TelemetryConfig{
+		LibraryName:               cfg.OtelLibraryName,
+		ServiceName:               cfg.OtelServiceName,
+		ServiceVersion:            cfg.OtelServiceVersion,
+		DeploymentEnv:             cfg.OtelDeploymentEnv,
+		CollectorExporterEndpoint: cfg.OtelColExporterEndpoint,
+		EnableTelemetry:           cfg.EnableTelemetry,
+		Logger:                    logger,
+	})
+
+	// Init rabbitmq connection
+	rabbitSource := fmt.Sprintf("%s://%s:%s@%s:%s",
+		cfg.RabbitURI, cfg.RabbitMQUser, cfg.RabbitMQPass, cfg.RabbitMQHost, cfg.RabbitMQPortAMQP)
+
+	logger.Infof(rabbitSource)
+
+	rabbitMQConnection := &libRabbitMQ.RabbitMQConnection{
+		ConnectionStringSource: rabbitSource,
+		HealthCheckURL:         cfg.RabbitMQHealthCheckURL,
+		Host:                   cfg.RabbitMQHost,
+		Port:                   cfg.RabbitMQPortHost,
+		User:                   cfg.RabbitMQUser,
+		Pass:                   cfg.RabbitMQPass,
+		Queue:                  cfg.RabbitMQGenerateReportQueue,
+		Logger:                 logger,
+	}
+
+	routes := rabbitmq.NewConsumerRoutes(rabbitMQConnection, cfg.RabbitMQNumWorkers, logger, telemetry)
+
+	// Config SeaweedFS connection
+	seaweedFSEndpoint := fmt.Sprintf("http://%s:%s", cfg.SeaweedFSHost, cfg.SeaweedFSFilerPort)
+	seaweedFSClient := simpleClient.NewSeaweedFSClient(seaweedFSEndpoint)
+
+	// Init mongo DB connection
+	escapedPass := url.QueryEscape(cfg.MongoDBPassword)
+	mongoSource := fmt.Sprintf("%s://%s:%s@%s:%s",
+		cfg.MongoURI, cfg.MongoDBUser, escapedPass, cfg.MongoDBHost, cfg.MongoDBPort)
+
+	if cfg.MaxPoolSize <= 0 {
+		cfg.MaxPoolSize = 100
+	}
+
+	mongoConnection := &mongoDB.MongoConnection{
+		ConnectionStringSource: mongoSource,
+		Database:               cfg.MongoDBName,
+		Logger:                 logger,
+		MaxPoolSize:            uint64(cfg.MaxPoolSize),
+	}
+
+	externalDataSeaweedFSRepository := external_data.NewSimpleRepository(seaweedFSClient, constant.ExternalDataBucketName)
+
+	// Initialize MongoDB repositories
+	externalConnectionMongoDBRepository := externalConnection.NewExternalConnectionMongoDBRepository(mongoConnection)
+
+	service := &services.UseCase{
+		ExternalDataSeaweedFS:     externalDataSeaweedFSRepository,
+		ExternalConnectionMongoDB: externalConnectionMongoDBRepository,
+		FileTTL:                   cfg.SeaweedFSTTL,
+	}
+
+	if cfg.SeaweedFSTTL != "" {
+		logger.Infof("Reports will expire after: %s", cfg.SeaweedFSTTL)
+	} else {
+		logger.Infof("Reports will be stored permanently (no TTL)")
+	}
+
+	licenseClient := libLicense.NewLicenseClient(
+		constant.ApplicationName,
+		cfg.LicenseKey,
+		cfg.OrganizationIDs,
+		&logger,
+	)
+	multiQueueConsumer := NewMultiQueueConsumer(routes, service)
+
+	return &Service{
+		MultiQueueConsumer: multiQueueConsumer,
+		Logger:             logger,
+		licenseShutdown:    licenseClient.GetLicenseManagerShutdown(),
+	}
+}
---
+++ pkg/constant/seaweedfs.go
@@ -0,0 +1,5 @@
+package constant
+
+const (
+	ExternalDataBucketName = "external-data"
+)
