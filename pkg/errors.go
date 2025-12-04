28c55ddeff6a333cac1271d6efef727cf11069ff
arthurkz <arthur.ribeiro@lerian.studio>
2025-12-04 12:18:42 -0300

    fix: Adjusting error type returned

--- pkg/errors.go
+++ pkg/errors.go
@@ -301,13 +301,13 @@ func ValidateBusinessError(err error, entityType string, args ...any) error {
 			Title:      "Invalid Date Format Error",
 			Message:    "The 'initialDate', 'finalDate', or both are in the incorrect format. Please use the 'yyyy-mm-dd' format and try again.",
 		},
-		constant.ErrInvalidFinalDate: EntityNotFoundError{
+		constant.ErrInvalidFinalDate: ValidationError{
 			EntityType: entityType,
 			Code:       constant.ErrInvalidFinalDate.Error(),
 			Title:      "Invalid Final Date Error",
 			Message:    "The 'finalDate' cannot be earlier than the 'initialDate'. Please verify the dates and try again.",
 		},
-		constant.ErrDateRangeExceedsLimit: EntityNotFoundError{
+		constant.ErrDateRangeExceedsLimit: ValidationError{
 			EntityType: entityType,
 			Code:       constant.ErrDateRangeExceedsLimit.Error(),
 			Title:      "Date Range Exceeds Limit Error",
@@ -319,19 +319,19 @@ func ValidateBusinessError(err error, entityType string, args ...any) error {
 			Title:      "Invalid Date Range Error",
 			Message:    "Both 'initialDate' and 'finalDate' fields are required and must be in the 'yyyy-mm-dd' format. Please provide valid dates and try again.",
 		},
-		constant.ErrPaginationLimitExceeded: EntityNotFoundError{
+		constant.ErrPaginationLimitExceeded: ValidationError{
 			EntityType: entityType,
 			Code:       constant.ErrPaginationLimitExceeded.Error(),
 			Title:      "Pagination Limit Exceeded",
 			Message:    fmt.Sprintf("The pagination limit exceeds the maximum allowed of %v items per page. Please verify the limit and try again.", args...),
 		},
-		constant.ErrInvalidSortOrder: EntityNotFoundError{
+		constant.ErrInvalidSortOrder: ValidationError{
 			EntityType: entityType,
 			Code:       constant.ErrInvalidSortOrder.Error(),
 			Title:      "Invalid Sort Order",
 			Message:    "The 'sort_order' field must be 'asc' or 'desc'. Please provide a valid sort order and try again.",
 		},
-		constant.ErrEntityNotFound: EntityNotFoundError{
+		constant.ErrEntityNotFound: ValidationError{
 			EntityType: entityType,
 			Code:       constant.ErrEntityNotFound.Error(),
 			Title:      "Entity Not Found",
